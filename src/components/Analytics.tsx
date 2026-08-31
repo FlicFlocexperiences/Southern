2b504cc7be440358a513adda4b94bc97b001ddb2
Boss Piyush <fanboiofharrystyles@gmail.com>
2026-08-31 16:05:10 +0530

    please work

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -71,6 +71,61 @@ export function Analytics() {
         }}
       />
 
+      {/* Meta Event Setup Tool (iwl.js) Compatibility Shim */}
+      <Script
+        id="meta-iwl-compat"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function () {
+              function safeRect(node) {
+                var el = node && node.nodeType === 1
+                  ? node
+                  : (node && node.parentElement) ||
+                    (node && node.nodeType === 9 && node.documentElement) ||
+                    null;
+                return el && typeof el.getBoundingClientRect === 'function'
+                  ? el.getBoundingClientRect()
+                  : { top: 0, left: 0, right: 0, bottom: 0, width: 0, height: 0, x: 0, y: 0 };
+              }
+
+              var targets = [
+                typeof Comment !== 'undefined' ? Comment : null,
+                typeof Text !== 'undefined' ? Text : null,
+                typeof DocumentFragment !== 'undefined' ? DocumentFragment : null,
+                typeof Document !== 'undefined' ? Document : null
+              ].filter(Boolean);
+
+              function applyPatch() {
+                targets.forEach(function (Ctor) {
+                  if (Ctor && !Ctor.prototype.getBoundingClientRect) {
+                    Ctor.prototype.getBoundingClientRect = function () { return safeRect(this); };
+                  }
+                });
+              }
+
+              // Apply patch only when Meta's Event Setup Tool (iwl.js) is detected in the DOM
+              var observer = new MutationObserver(function (mutations) {
+                for (var i = 0; i < mutations.length; i++) {
+                  var addedNodes = mutations[i].addedNodes;
+                  for (var j = 0; j < addedNodes.length; j++) {
+                    var node = addedNodes[j];
+                    if (node && node.tagName === 'SCRIPT' && /iwl\\.js/.test(node.src || '')) {
+                      applyPatch();
+                      observer.disconnect();
+                      return;
+                    }
+                  }
+                }
+              });
+              if (document.documentElement) {
+                observer.observe(document.documentElement, { childList: true, subtree: true });
+              }
+            })();
+          `,
+        }}
+      />
+
       {/* Meta / Facebook Pixel */}
       <Script
         id="meta-pixel"
